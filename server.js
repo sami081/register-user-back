eeb99da89f9100f3a36d485d450b3faa3021863c
sami081 <makhloufisami0811@gmail.com>
2024-07-29 12:12:50 +0200

    modif cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://register-g136r6fuw-sami081s-projects.vercel.app/",
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
 };
